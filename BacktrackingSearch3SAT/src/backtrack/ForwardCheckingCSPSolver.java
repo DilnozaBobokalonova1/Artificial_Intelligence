23dcf437a0f477febf6a45f7be5f1ad466897af3
Dilnoza Bobokalonova <dilnozabobokalonova1@gmail.com>
2017-11-05 18:05:27 -0500

    Update ForwardCheckingCSPSolver.java

--- BacktrackingSearch3SAT/src/backtrack/ForwardCheckingCSPSolver.java
+++ BacktrackingSearch3SAT/src/backtrack/ForwardCheckingCSPSolver.java
@@ -17,18 +17,7 @@ public class ForwardCheckingCSPSolver extends BacktrackingCSPSolver {
 	 */
 	@Override
 	protected <E> Inference<E> inference(CSP<E> csp, Assignment<E> assignment, String var, E value) {
-		/* TODO
-		 * Implement the forward checking. You may want to iterate over all
-		 * constraints to identify those who involve the given variable. Then,
-		 * iterate over the variables of the scope of the constraint and check
-		 * if the variable is not yet assigned. If it is not assigned, check all
-		 * the values of the domain of that variable, and identify those values
-		 * that are inconsistent with the constraint (therefore, you might temporarily
-		 * modify the assignment with the value to test, and restore the assignment
-		 * later on). The inconsistent values should be added to the inference that
-		 * will be returned. If no value was found at all, then return failure (null in this
-		 * case).
-		 */
+	
 		Inference<E> inference = new Inference<E>();
 		
 		for(Constraint c : csp.constraints) {
